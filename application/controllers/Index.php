dde0f6cec44544cd948fa8aeee3a1912cea1d495
nurdiantoro10 <69132810+nurdiantoro10@users.noreply.github.com>
2022-05-07 13:35:46 +0700

    versi 1.0
    Instal CI3

---
+++ application/controllers/Index.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Index extends CI_Controller
+{
+
+    public function index()
+    {
+        // $this->load->view('header');
+        $this->load->view('index');
+        // $this->load->view('footer');
+    }
+}
