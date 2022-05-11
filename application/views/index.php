36d66e24a556fddf208bf0521793e333c72966c9
nurdiantoro10 <69132810+nurdiantoro10@users.noreply.github.com>
2022-05-11 23:59:13 +0700

    versi 1.1
    -instal tailwindcss
    -pembuatan navbar

--- application/views/index.php
+++ application/views/index.php
@@ -8,10 +8,30 @@
     <title>Website Resmi DPRD Banggai Laut</title>
 
     <link rel="icon" type="image/x-icon" href="<?= base_url('') ?>/assets/img/favicon.ico">
+
+    <!-- CSS -->
+    <!-- <link href="<?= base_url('') ?>assets/style.css" rel="stylesheet"> -->
+    <!-- <link href="<?= base_url('') ?>assets/fontawesome/css/all.min.css" rel="stylesheet"> -->
+    <!-- <link href="<?= base_url('') ?>assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="<?= base_url('') ?>assets/style.css" rel="stylesheet">
+    <!-- <link href="<?= base_url('') ?>assets/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"> -->
+    <!-- <link href="<?= base_url('') ?>assets/owlcarousel/assets/owl.theme.default.min.css" rel="stylesheet"> -->
+    <!-- <link href="<?= base_url('') ?>assets/datatables/datatables.min.css" rel="stylesheet" type="text/css" /> -->
 </head>
 
 <body>
-    <?= base_url() ?>
+    <nav class="bg-white px-14 py-3 border-b-2">
+        <div>
+            <img class="h-8" src="<?= base_url('') ?>/assets/img/Logo-Kabupaten-Banggai-Laut.png" alt="logo-banggai-laut">
+        </div>
+        <div>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 
 </html>
