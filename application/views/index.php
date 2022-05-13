e862fe16fe8952e046c9e17267540d8900568666
nurdiantoro10 <69132810+nurdiantoro10@users.noreply.github.com>
2022-05-14 02:30:22 +0700

    Update 1.2
    masih berkelut di homepage

--- application/views/index.php
+++ application/views/index.php
@@ -10,17 +10,17 @@
     <link rel="icon" type="image/x-icon" href="<?= base_url('') ?>/assets/img/favicon.ico">
 
     <!-- CSS -->
-    <link href="<?= base_url('assets/style.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/fontawesome/css/all.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/owlcarousel/assets/owl.carousel.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/owlcarousel/assets/owl.theme.default.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/datatables/datatables.min.css') ?>" rel="stylesheet" type="text/css" />
+    <link href="<?= base_url('') ?>assets/style.css" rel="stylesheet">
+    <link href="<?= base_url('') ?>assets/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="<?= base_url('') ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('') ?>assets/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?= base_url('') ?>assets/owlcarousel/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link href="<?= base_url('') ?>assets/datatables/datatables.min.css" rel="stylesheet" type="text/css" />
 
     <!-- JS -->
-    <script src="<?= base_url('assets/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('assets/owlcarousel/owl.carousel.min.js') ?>"></script>
-    <script src="<?= base_url('assets/datatables/datatables.min.js') ?>"></script>
+    <script src="<?= base_url('') ?>assets/jquery.min.js"></script>
+    <script src="<?= base_url('') ?>assets/owlcarousel/owl.carousel.min.js"></script>
+    <script src="<?= base_url('') ?>assets/datatables/datatables.min.js"></script>
 
 </head>
 
@@ -66,8 +66,74 @@
 
     <div id="home">
         <div class="header">
-            <img src="<?= base_url('') ?>assets/img/home.png" class="img-fluid">
+            <div class="header-content text-center">
+                <h1 class="white-100">SELAMAT DATANG DI</h1>
+                <h1 class="warna-01">DPRD KABUPATEN BANGGAI LAUT</h1>
+                <P class="white-100 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh imperdiet sollicitudin etiam sem. Elementum urna maecenas tellus risus sapien urna. Blandit pellentesque egestas tellus at luctus turpis nunc.</P>
+            </div>
         </div>
+        <section id="berita-utama">
+            <div class="container">
+                <h2 class="warna-01 text-center">BERITA UTAMA</h2>
+                <div class="row">
+                    <div class="col-md-9">
+                        <a href="">
+                            <div class="card">
+                                <div class="background-wrapper" style="background-image: url('assets/img/image-1.png') ;">
+                                </div>
+                                <div class="card-content">
+                                    <span class="tanggal">19 Januari 2022</span>
+                                    <p class="judul">
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem iste tempora quod cumque saepe voluptatum unde recusandae dolorum animi aut, placeat aliquid quia illum impedit obcaecati vitae hic accusamus ratione.
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="row h-100">
+                            <div class="col-md-12 mb-4">
+                                <a href="">
+                                    <div class="card">
+                                        <div class="background-wrapper" style="background-image: url('assets/img/image-1.png') ;">
+                                        </div>
+                                        <div class="card-content">
+                                            <span class="tanggal">19 Januari 2022</span>
+                                            <p class="judul">
+                                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-12">
+                                <a href="">
+                                    <div class="card">
+                                        <div class="background-wrapper" style="background-image: url('assets/img/image-1.png') ;">
+                                        </div>
+                                        <div class="card-content">
+                                            <span class="tanggal">19 Januari 2022</span>
+                                            <p class="judul">
+                                                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mt-3">
+                    <a href="" class="read-more">lihat semua berita</a>
+                </div>
+            </div>
+        </section>
+
+        <section id="banner">
+            <div class="container">
+                <img class="banner" src="<?= base_url() ?>/assets/img/image-2.png" alt="banner">
+            </div>
+        </section>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
